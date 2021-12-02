53d32647dc48dbeee97177dce270957dbf21520f
Prieto-Mayca-A <68166806+Prieto-Mayca-A@users.noreply.github.com>
2021-12-02 17:28:32 -0500

    lab5 commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace Lab05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtEnrollmentDate = new System.Windows.Forms.DateTimePicker();
+            this.txtHireDate = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPersonID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.dgvListado = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.txtEnrollmentDate);
+            this.panel1.Controls.Add(this.txtHireDate);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtLastName);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtFirstName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtPersonID);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(340, 219);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtEnrollmentDate
+            // 
+            this.txtEnrollmentDate.AccessibleRole = System.Windows.Forms.AccessibleRole.Alert;
+            this.txtEnrollmentDate.CalendarForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtEnrollmentDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtEnrollmentDate.ImeMode = System.Windows.Forms.ImeMode.Katakana;
+            this.txtEnrollmentDate.Location = new System.Drawing.Point(179, 185);
+            this.txtEnrollmentDate.Name = "txtEnrollmentDate";
+            this.txtEnrollmentDate.Size = new System.Drawing.Size(129, 20);
+            this.txtEnrollmentDate.TabIndex = 11;
+            // 
+            // txtHireDate
+            // 
+            this.txtHireDate.AccessibleRole = System.Windows.Forms.AccessibleRole.Alert;
+            this.txtHireDate.CalendarForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtHireDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtHireDate.ImeMode = System.Windows.Forms.ImeMode.Katakana;
+            this.txtHireDate.Location = new System.Drawing.Point(22, 185);
+            this.txtHireDate.Name = "txtHireDate";
+            this.txtHireDate.Size = new System.Drawing.Size(127, 20);
+            this.txtHireDate.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(176, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Inscripcion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Contrato";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(179, 117);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(129, 20);
+            this.txtLastName.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Apellido";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(22, 118);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(127, 20);
+            this.txtFirstName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            // 
+            // txtPersonID
+            // 
+            this.txtPersonID.Location = new System.Drawing.Point(22, 46);
+            this.txtPersonID.Name = "txtPersonID";
+            this.txtPersonID.Size = new System.Drawing.Size(79, 20);
+            this.txtPersonID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 6);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Persona";
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(375, 27);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 13;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(375, 69);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 14;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(375, 112);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertar.TabIndex = 15;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(375, 151);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 16;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(375, 194);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 17;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // dgvListado
+            // 
+            this.dgvListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListado.Location = new System.Drawing.Point(12, 247);
+            this.dgvListado.Name = "dgvListado";
+            this.dgvListado.ReadOnly = true;
+            this.dgvListado.RowHeadersVisible = false;
+            this.dgvListado.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvListado.Size = new System.Drawing.Size(453, 188);
+            this.dgvListado.TabIndex = 18;
+            this.dgvListado.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvListado_CellContentClick);
+            this.dgvListado.SelectionChanged += new System.EventHandler(this.dgvListado_SelectionChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 449);
+            this.Controls.Add(this.dgvListado);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Mantenimiento de Personas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker txtEnrollmentDate;
+        private System.Windows.Forms.DateTimePicker txtHireDate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPersonID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DataGridView dgvListado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab05
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection con;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String str = "Server=DESKTOP-6TPRNVU\\LOCAL;DataBase=School;Integrated Security=true";
+            con = new SqlConnection(str);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            String sp = "DeletePerson";
+            SqlCommand cmd = new SqlCommand(sp, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@PersonID", txtPersonID.Text);
+
+            int resultado = cmd.ExecuteNonQuery();
+            if (resultado > 0)
+            {
+                MessageBox.Show("Se ha eliminado el registro correctamente ");
+            }
+            con.Close();
+        }   
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            String sql = "SELECT * FROM Person";
+            SqlCommand cmd = new SqlCommand(sql,con);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+
+            dgvListado.DataSource = dt;
+            con.Close();
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            String sp = "InsertPerson";
+            SqlCommand cmd = new SqlCommand(sp, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+            cmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+            cmd.Parameters.AddWithValue("@HireDate", Convert.ToDateTime(txtHireDate.Text));
+            cmd.Parameters.AddWithValue("@EnrollmentDate", Convert.ToDateTime(txtEnrollmentDate.Text));
+
+            int codigo = Convert.ToInt32(cmd.ExecuteScalar());
+            MessageBox.Show("Se ha registrado una nueva persona con el codigo " + codigo);
+            con.Close();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            String sp = "UpdatePerson";
+            SqlCommand cmd = new SqlCommand(sp, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@PersonID", txtPersonID.Text);
+            cmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+            cmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+            cmd.Parameters.AddWithValue("@HireDate", txtHireDate.Text);
+            cmd.Parameters.AddWithValue("@EnrollmentDate", txtEnrollmentDate.Text);
+
+            int resultado = cmd.ExecuteNonQuery();
+            if (resultado > 0)
+            {
+                MessageBox.Show("Se ha modificado el registro correctamente ");
+            }
+            con.Close();
+        }
+
+        private void dgvListado_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            String FirstName = txtFirstName.Text;
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "BuscarPersonaNombre";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = con;
+
+            SqlParameter param = new SqlParameter();
+            param.ParameterName = "@FirstName";
+            param.SqlDbType = SqlDbType.NVarChar;
+            param.Value = FirstName;
+
+            cmd.Parameters.Add(param);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            dgvListado.DataSource = dt;
+            dgvListado.Refresh();
+
+            con.Close();
+        }
+
+        private void dgvListado_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvListado.SelectedRows.Count > 0)
+            {
+                txtPersonID.Text = dgvListado.SelectedRows[0].Cells[0].Value.ToString();
+                txtFirstName.Text = dgvListado.SelectedRows[0].Cells[1].Value.ToString();
+                txtLastName.Text = dgvListado.SelectedRows[0].Cells[2].Value.ToString();
+                txtHireDate.Text = dgvListado.SelectedRows[0].Cells[3].Value.ToString();
+                txtEnrollmentDate.Text = dgvListado.SelectedRows[0].Cells[4].Value.ToString();
+            }
+            
+        }
+    }
+}
